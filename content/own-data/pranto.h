226eabb0caeab3c2b949948e0569110fd90c4c57
Md. Arik Rayhan <mdarikrayhan@gmail.com>
2024-03-08 06:37:38 +0000

    commit

--- content/own-data/pranto.h
+++
@@ -1,8 +0,0 @@
-/**
- * Author: Md. Arik Rayhan
- * Date: 2023-11-01
- * License: CC0
- * Description: This is a template for creating own data structure.
- * Time: O(n)
- * Status: Tested on codeforces
- */
